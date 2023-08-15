1056a3ddf7748867213d17367f99bd28e791cc34
309 <309@academicos.cesde.edu.co>
2023-08-15 08:06:12 -0500

    feat: creamos variables primitivas y lista de datos

---
+++ ejemplo_1.py
@@ -0,0 +1,13 @@
+#hola
+nombreUsuario = "simon salazar castaño"
+edadDelUsuario = 19
+estaturaUsuario = 1.87
+esSkater = True
+
+print(f"su nombre es: {nombreUsuario} y tiene {edadDelUsuario} años.")
+
+#lista
+#la lista se crea con corchetes
+comidasFavoritas = ["hambuerguesa", "papitas", "bandeja paisa"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
