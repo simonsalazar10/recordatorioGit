43d422e25160fb05527c48dbe054b7c2d9b96895
309 <309@academicos.cesde.edu.co>
2023-08-15 08:37:11 -0500

    feat: se reciben datos por teclado y se castea

--- ejemplo_1.py
+++ ejemplo_1.py
@@ -1,5 +1,5 @@
 #hola
-nombreUsuario = "simon salazar castaño"
+""" nombreUsuario = "simon salazar castaño"
 edadDelUsuario = 19
 estaturaUsuario = 1.87
 esSkater = True
@@ -11,3 +11,13 @@ print(f"su nombre es: {nombreUsuario} y tiene {edadDelUsuario} años.")
 comidasFavoritas = ["hambuerguesa", "papitas", "bandeja paisa"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#entradas por teclado
+lugarTrabajoUsuario = input("Digita el lugar de trabajo: ")
+print(f"usted trabaja en: {lugarTrabajoUsuario} ") """
+
+#pedimos dos numeros
+numeroUno = int(input("digita un numero: "))
+numeroDos = int(input("digita un numero: "))
+sumatoria = numeroUno + numeroDos
+print(f"la suma de {numeroUno} + {numeroDos} = {sumatoria}")
